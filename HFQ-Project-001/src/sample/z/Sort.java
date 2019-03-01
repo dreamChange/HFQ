5ac490e262883a12d87bfcca27cc8256a8fcc759
wenpan.lei@outlook.com <wenpan.lei@outlook.com>
2019-03-01 09:26:25 +0800

    第二次提交

--- HFQ-Project-001/src/sample/z/Sort.java
+++ HFQ-Project-001/src/sample/z/Sort.java
@@ -9,12 +9,14 @@ public class Sort {
 
 //		int[] initArr = Sort.init(20,80);
 
-		int[] initArr = {-66, 44, -99, 22, -11, -55 };
-		
-		
+		int[] initArr = {-66, 44, -99, 2,-5,-99,77};
 
+		print(iSort(initArr));
+		
 //		print(mSort(initArr));
 	}
+	
+	
 	
 	// 随机数组
 	public static int[] init(int initNum,int nextNum) {
@@ -69,6 +71,54 @@ public class Sort {
 	 */
 	public static int[] iSort(int[] initArr) {
 		
-		return initArr;
+		int[] newArr = new int[initArr.length];
+		
+		for (int i = 0; i < initArr.length; i++) {
+			int tmp = initArr[i];
+			//初始化
+			if(i == 0){
+				newArr[0] = tmp;
+				continue;
+			}
+			//记录下标值
+			int subScript = 0;
+			//需要插入标识
+			boolean ifInsert= false;
+			//插入数据;
+			int iTmp = 0;
+			
+			for (int j = 0; j < newArr.length; j++) {
+				
+				//1.寻找插入下标值；
+				int tmp2 = newArr[j];
+				
+				//插入标识为true 时，需将后续数据往后移动1下标值
+				if(ifInsert){
+					newArr[subScript] = iTmp;
+					iTmp = tmp2;
+					subScript++;
+				}
+				//寻找到时，记录当前下标值
+				else if(tmp < tmp2){
+					iTmp = tmp;
+					subScript = j;
+					ifInsert = true;
+					j--;
+					continue;
+				}
+				//当前数据为最大值处理方式
+				else if(i==j){
+					iTmp = tmp;
+					subScript = j;
+					ifInsert = true;
+					j--;
+					continue;
+				}
+				else{
+					//nothing
+				}
+			}
+		}
+		return newArr;
 	}
 }
